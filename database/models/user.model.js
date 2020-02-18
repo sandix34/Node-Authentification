7256d970df59bbe64b3028c877cacf2b06618afe
sandrine <sandrinezocca@gmail.com>
2020-02-18 15:24:09 +0100

    🔑  password hash

--- database/models/user.model.js
+++ database/models/user.model.js
@@ -10,6 +10,22 @@ const userSchema = schema({
   username: String
 })
 
+// méthode statique sur le modèle pour hacher les mots de passe
+userSchema.statics.hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  } catch(e) {
+    throw(e)
+  }
+}
+
+// méthode pour comparer les mots de passe de connxion et dans la base de données
+// méthode non statique car elle sera utilisée sur les instances user.
+// this.local.password fait référence à l'intance user sur laquelle on l'utilisera
+userSchema.methods.comparePassword = function(password) {
+  return bcrypt.compare(password, this.local.password);
+}
 
 const User = mongoose.model('user', userSchema);
 
