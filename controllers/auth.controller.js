cdda56998b19641e87d665b70063d99246677e99
sandrine <sandrinezocca@gmail.com>
2020-02-18 14:38:12 +0100

    ⚙️ creation of controllers

---
+++ controllers/auth.controller.js
@@ -0,0 +1,14 @@
+exports.sessionNew = (req, res, next) => {
+  // TODO
+  res.end();
+}
+
+exports.sessionCreate = (req, res, next) => {
+  // TODO
+  res.end();
+}
+
+exports.sessionDelete = (req, res, next) => {
+  // TODO
+  res.end();
+}
---
+++ controllers/user.controller.js
@@ -0,0 +1,9 @@
+exports.userNew = (req, res, next) => {
+  // TODO
+  res.end();
+}
+
+exports.userCreate = async (req, res, next) => {
+  // TODO
+  res.end();
+}
