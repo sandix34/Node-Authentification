a294e13ab12d295066e298570ee0f523c61193dc
sandrine <sandrinezocca@gmail.com>
2020-02-20 17:14:53 +0100

    ⛔  error handling

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,30 @@
+const passport = require('passport');
+
 exports.sessionNew = (req, res, next) => {
   res.render('signin', { error: null });
 }
 
 exports.sessionCreate = (req, res, next) => {
-  // TODO
-  res.end();
+  // premier paramètre --> authentification avec la stratégie local
+  // dauxilème paramètre --> une fonction de callback 
+  passport.authenticate('local', (err, user, info) => {
+    // si une erreur serveur, passe next pour rediriger vers le middleware de gestion d'erreur
+    if (err) {
+      next(e);
+      // si pas d'utilisateur, réaffiche le formulaire de connexion avec message d'erreur
+    } else if (!user) {
+      res.render('signin', { error: info.message })
+      // sinon créer une session pour l'utilisateur connecté et redirige sur la page d'accueil
+    } else {
+      req.login(user, (err) => {
+        if (err) { 
+          next(e) 
+        } else {
+          res.redirect('/');
+        }
+      })
+    }
+  })(req, res , next);
 }
 
 exports.sessionDelete = (req, res, next) => {
