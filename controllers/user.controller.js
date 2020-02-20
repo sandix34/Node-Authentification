67f63d6d506741d9cde5a2360c11c9dcf6e5a366
sandrine <sandrinezocca@gmail.com>
2020-02-20 18:13:29 +0100

    ✔️ automatic user login when registering

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,7 +8,10 @@ exports.userCreate = async (req, res, next) => {
   try {
     const body = req.body;
     const user = await createUser(body);
-    res.redirect('/');
+    req.login(user, (err) => {
+      if (err) { next(err) }
+      res.redirect('/');
+    })
   } catch(e) {
     res.render('signup', { error: e.message });
   }
